adf7a204b13ff3000e95e61e7e338f8a003b2b01
Elaine Li <zl4490@nyu.edu>
2025-03-03 17:54:45 -0500

    set the BACKEND_URL to use REACT_APP_URL

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# Run your React frontend against a local API server.
+# Run your React frontend against a cloud API server.
 export REACT_APP_URL_PRE="https://znelaine.pythonanywhere.com/"
 npm start
