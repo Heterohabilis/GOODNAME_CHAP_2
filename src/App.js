5f3315cc6e97c519032f810414c984b42915576e
yuzukaR <rao18511696926@gmail.com>
2025-04-21 12:14:59 -0400

    change navbar ui

--- src/App.js
+++ src/App.js
@@ -1,48 +1,64 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  // useParams,
-} from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import Navbar from './Components/Navbar';
 import People from './Components/People';
 import Manuscripts from './Components/Manuscript';
-// import Texts from './Components/Texts'
-import About from './Components/About'
+import About from './Components/About';
 import PersonPage from './Components/PersonPage';
-import Masthead from "./Components/Masthead";
-import Home from "./Components/Home";
-import Login from "./Components/Login";
-
-// function PersonPage() {
-//   const { name } = useParams();
-//   return <h1>{name}</h1>
-// }
-
+import Masthead from './Components/Masthead';
+import Home from './Components/Home';
+import Login from './Components/Login';
 
 function App() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleNavToggle = (open) => {
+    setIsNavOpen(open);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {/* For a different home page, do:
-         <Route index element={<Login />} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="people" element={<People />} />
-        <Route path="people/:email" element={<PersonPage />} />
-        <Route path="masthead" element={<Masthead />} />
-        <Route path="manuscripts" element={<Manuscripts />} />
-        {/*<Route path="texts" element={<Texts />} />*/}
-        <Route path="about" element={<About />} />
-        <Route path="login" element={<Login />} />
-      </Routes>
+      <div className="app-container">
+        <header className="top-bar">
+          <div 
+            className="nav-toggle-icon" 
+            onMouseEnter={() => handleNavToggle(true)}
+            onClick={() => handleNavToggle(!isNavOpen)}
+          >
+            &#9776;
+          </div>
+          <h1 className="site-title">GOODNAME JOURNAL</h1>
+        </header>
+
+        <nav 
+          className={`side-navbar ${isNavOpen ? 'open' : ''}`}
+          onMouseLeave={() => handleNavToggle(false)}
+        >
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/people">People</Link></li>
+            <li><Link to="/manuscripts">Manuscripts</Link></li>
+            <li><Link to="/masthead">Masthead</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="people" element={<People />} />
+            <Route path="people/:email" element={<PersonPage />} />
+            <Route path="masthead" element={<Masthead />} />
+            <Route path="manuscripts" element={<Manuscripts />} />
+            <Route path="about" element={<About />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
 
 export default App;
-
